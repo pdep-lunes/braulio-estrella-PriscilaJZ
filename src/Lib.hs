ba4488fed96803b25513466abec59c234e478251
PriscilaJZ <pzarate@frba.utn.edu.ar>
2025-04-28 11:43:57 -0300

    Agrego funciones utiles y primera version de bolaEspinosa

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,5 +13,13 @@ Espina = "Espina", "bola de espinas", "granadas de espinas", True, 4800
 Pamela :: Personaje
 Pamela = "Pamela" "lluvia de tuercas" "torreta Curativa" False 9600
 
-bolaEspinosa :: Int->Int
-bolaEspinosa =  
+vidaDePersonaje :: [Personaje] -> Int
+vidaDePersonaje (_, _, _, _, vida) = vida
+
+vidaEsMenorA :: [Personaje] -> Int -> Bool
+vidaEsMenorA personaje danio = (vidaDePersonaje personaje) < danio
+
+bolaEspinosa :: [Personaje]->Int
+bolaEspinosa personaje 
+  |vidaEsMenorA personaje 1000 = vidaDePersonaje personaje = 0
+  |otherwise (vidaDePersonaje personaje) - 1000 
