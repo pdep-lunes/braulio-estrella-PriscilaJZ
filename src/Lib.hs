ae507f44df36ead5768950a8e18696199dba980b
PriscilaJZ <pzarate.frba.utn.edu.ar>
2025-04-28 11:21:34 -0300

    Agrego personajes Pamela y Espina

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,3 +4,14 @@ import Text.Show.Functions ()
 
 doble :: Int -> Int
 doble x = x * 2
+
+type Personaje=(String, String, String, Bool, Int)
+
+Espina :: Personaje
+Espina = "Espina", "bola de espinas", "granadas de espinas", True, 4800
+
+Pamela :: Personaje
+Pamela = "Pamela" "lluvia de tuercas" "torreta Curativa" False 9600
+
+bolaEspinosa :: Int->Int
+bolaEspinosa =  
