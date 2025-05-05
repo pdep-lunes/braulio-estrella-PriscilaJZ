fbdc347e5622a7c5736e04f701ca690635fc5e6c
PriscilaJZ <pzarate@frba.utn.edu.ar>
2025-05-04 21:20:20 -0300

    Agrego funciones ataqueConPoderEspecial y correcciones

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,10 @@
-module Lib () where
+module Lib (Personaje(..), espina, pamela, personajes, curar, herir, bolaEspinosa, amigo, lluviaDeTuercas,
+ torretaCurativa, granadaDeEspinas, esVidaMenorA800, quienTieneMenosDe800HP) where
 
 import Text.Show.Functions ()
 
-doble :: Int -> Int
-doble x = x * 2
+--doble :: Int -> Int
+--doble x = x * 2
 
 data Personaje = Personaje {
     nombre :: String,
@@ -14,11 +15,14 @@ data Personaje = Personaje {
     vidaInicial :: Int
 } deriving Show
 
-Espina :: Personaje
-Espina = ("Espina", "bola de espinas", "granadas de espinas", True, 4800, 4800)
+espina :: Personaje
+espina = Personaje "Espina" "bola de espinas" "granadas de espinas" True 4800 4800
 
-Pamela :: Personaje
-Pamela = ("Pamela", "lluvia de tuercas", "torreta Curativa", False, 9600, 9600)
+pamela :: Personaje
+pamela = Personaje "Pamela" "lluvia de tuercas" "torreta curativa" False 9600 9600
+
+personajes :: [Personaje]
+personajes = [pamela, espina]
 
 curar :: Personaje -> Int -> Personaje
 curar personaje sanacion = personaje {vida = min (vida personaje + sanacion) (vidaInicial personaje)}
@@ -30,19 +34,43 @@ bolaEspinosa :: Personaje -> Personaje
 bolaEspinosa personaje = herir personaje 1000
 
 amigo :: String -> Bool
-amigo amigoOenemigo = amigoOenemigo == "colega"
+amigo amigoOEnemigo = amigoOEnemigo == "colega"
 
-lluviaDeTuercas :: Personaje -> String -> Personaje
-lluviaDeTuercas personaje amigoOenemigo = 
-  |amigo amigoOenemigo = curar personaje 800
-  |otherwise = herir personaje 800
+lluviaDeTuercas :: String -> Personaje -> Personaje
+lluviaDeTuercas amigoOEnemigo personaje
+  | amigo amigoOEnemigo = curar personaje 800
+  | otherwise = herir personaje 800
 
-torretaCurativa :: Personaje -> Bool -> Personaje
-torretaCurativa personaje superActivo = personaje {superActivo = True} && personaje {vida = vida personaje * 2}
+torretaCurativa :: Personaje -> Personaje
+torretaCurativa personaje = personaje {superActivo = True, vida = min (vida personaje * 2) (vidaInicial personaje)}
 
-granadaDeEspinas :: Personaje 
-granadaDeEspinas personaje radio 
-  |radio > 3 = personaje {nombre = nombre personaje + "Espina estuvo aquí"} &&
-    |vida personaje < 800 = personaje {vida = 0} && personaje {superActivo = False}
-  |otherwise = bolaEspinosa personaje
-  
+granadaDeEspinas ::  Int -> Personaje -> Personaje
+granadaDeEspinas radio personaje 
+  | radio > 3 && vida personaje < 800 = personaje {nombre = nombre personaje ++ "Espina estuvo aquí", vida = 0, superActivo = False}
+  | radio > 3 = personaje {nombre = nombre personaje ++ "Espina estuvo aquí"}
+  | otherwise = bolaEspinosa personaje
+
+esVidaMenorA800 :: Personaje -> Bool
+esVidaMenorA800 personaje = vida personaje < 800
+
+quienTieneMenosDe800HP :: [Personaje] -> [Personaje]
+quienTieneMenosDe800HP listaDePersonajes = filter esVidaMenorA800 listaDePersonajes
+
+lluviaDeTuercas "enemigo" :: Personaje -> Personaje
+
+ataqueConSuperPoder :: Int -> Personaje -> Personaje -> Personaje
+ataqueConSuperPoder radio personaje personajeEnemigo
+  | superPoder personaje == "granadas de espinas" = granadaDeEspinas personajeEnemigo radio
+  | superPoder personaje == "torreta curativa" = torretaCurativa personajeEnemigo
+  | otherwise = personajeEnemigo
+
+ataqueConBasico :: Personaje -> Personaje -> Personaje
+ataqueConBasico personaje personajeEnemigo
+  | poderBasico personaje == "bola de espinas" = bolaEspinosa personajeEnemigo
+  | poderBasico personaje == "lluvia de tuercas" = lluviaDeTuercas "enemigo" personajeEnemigo
+  | otherwise = personajeEnemigo 
+
+ataqueConPoderEspecial :: Int -> Personaje -> Personaje -> Personaje
+ataqueConPoderEspecial radio personaje personajeEnemigo
+  | superActivo personaje == True = ataqueConBasico personaje (ataqueConSuperPoder radio personaje personajeEnemigo)
+  | otherwise = personajeEnemigo
